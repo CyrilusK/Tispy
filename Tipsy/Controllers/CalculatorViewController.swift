b6453b698c1893281577f47ffdc7f7f5b2a2056d
CyrilusK <kardash199696@mail.ru>
2022-06-29 20:55:46 +0300

    added logic for getting input data

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -7,18 +7,29 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
+    var values = Data()
     
     @IBAction func tipChanged(_ sender: UIButton) {
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        sender.isSelected = true
+        guard let title = sender.currentTitle else { return }
+        values.precent = Float(title.dropLast())! / 100.0
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
+        values.numberOfPeople = Int(sender.value)
     }
-    
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        guard let bill = Double(billTextField.text!) else {
+            return billTextField.text = ""
+        }
+        values.billTotal = bill
+        print(values.billTotal)
     }
-    
-
 
 }
 
---
+++ Tipsy/Model/Data.swift
@@ -0,0 +1,6 @@
+
+struct Data {
+    var precent: Float = 0
+    var numberOfPeople: Int = 0
+    var billTotal : Double = 0
+}
